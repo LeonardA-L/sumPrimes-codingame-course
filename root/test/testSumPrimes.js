a408c5e06873a966b6ece64b5f62e952e06758e0
Leonard Allain-Launay <leonard@codingame.com>
2017-03-17 15:01:33 -0700

    Add actual tests

--- root/test/testSumPrimes.js
+++ root/test/testSumPrimes.js
@@ -1,15 +1,23 @@
 var sumPrimes = require('../sumPrimes');
 var assert = require('assert');
 
-console.log(sumPrimes(3))
-console.log(sumPrimes(10))
-console.log(sumPrimes(50))
-console.log(sumPrimes(100))
-console.log(sumPrimes(103))
-console.log(sumPrimes(10000))
-
 describe('SumPrimes Function', function() {
-  it('should return true when the parameter is 5', function() {
-    assert.equal(isPrime(5), true);
+  it('should return 2 when the parameter is 3', function() {
+    assert.equal(isPrime(3), 2);
+  });
+  it('should return 17 when the parameter is 10', function() {
+    assert.equal(isPrime(10), 17);
+  });
+  it('should return 328 when the parameter is 50', function() {
+    assert.equal(isPrime(50), 328);
+  });
+  it('should return 1060 when the parameter is 100', function() {
+    assert.equal(isPrime(100), 1060);
+  });
+  it('should return 1161 when the parameter is 103', function() {
+    assert.equal(isPrime(103), 1161);
+  });
+  it('should return 5736396 when the parameter is 10000', function() {
+    assert.equal(isPrime(10000), 5736396);
   });
 });
